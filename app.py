69dc19da96427575d75dc06ad5f8bfe04472e4e2
Coding-with-Adam <adam@charming-data.com>
2024-02-23 21:55:39 -0500

    new keys

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import playsound
 from dotenv import find_dotenv, load_dotenv
 
 dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)  # load api keys
+load_dotenv(dotenv_path)  # load api ke
 
 # Create a bot instance
 # os.environ["OPENAI_API_KEY"] = "key"
